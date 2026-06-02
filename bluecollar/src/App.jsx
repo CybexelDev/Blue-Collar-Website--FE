975ab497b35e033b2a42ba62c3b772f1e6d12c8b
CybexelDev <developerscbe.cybexel@gmail.com>
2026-06-02 17:29:18 +0530

    Added new changes

--- bluecollar/src/App.jsx
+++ bluecollar/src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import JobSearchHero from './Layout/Home/Hero/HomeBanner'
+import Featured from './Components/FeaturedJobs/Featured'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <>
       <Navbar/>
       <JobSearchHero/>
+      <Featured /> 
     </>
   )
 }
